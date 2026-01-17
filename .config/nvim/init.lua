7655874901d4b9f76e8225af01c18a5989d50a02
Lakshmanshankar <lakshmanshankarc@gmail.com>
2026-01-17 15:40:30 +0530

    update buffer telescope keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
-
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
@@ -55,3 +54,6 @@ vim.opt.fillchars = { fold = " " }
 vim.opt.foldmethod = "indent"
 vim.opt.foldenable = false
 vim.opt.foldlevel = 99
+
+vim.opt.mouse = "a"
+vim.opt.mousemodel = "extend"
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -35,3 +35,19 @@ map("n", "<leader>tw", "<cmd>set wrap!<CR>", { desc = "Toggle wrap" })
 
 map("n", "<C-d>", "<C-d>zz<CR>", { silent = true, noremap = true })
 map("n", "<C-u>", "<C-u>zz<CR>", { silent = true, noremap = true })
+
+if require("nvconfig").ui.tabufline.enabled then
+  map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+
+  map("n", "<tab>", function()
+    require("nvchad.tabufline").next()
+  end, { desc = "buffer goto next" })
+
+  map("n", "<S-tab>", function()
+    require("nvchad.tabufline").prev()
+  end, { desc = "buffer goto prev" })
+
+  map("n", "<leader>x", function()
+    require("nvchad.tabufline").close_buffer()
+  end, { desc = "buffer close" })
+end
